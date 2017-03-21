b4b6e82151cc684b11b22a96b95f9ca7f9e963a7
Evan Linde <elinde@okstate.edu>
2017-03-21 12:04:40 -0500

    add index

--- auto-push.sh
+++ auto-push.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 COMMIT_MESSAGE=" - auto_push.sh"
 DATE=$(date "+%Y-%m-%d")
-
-[ full_terminal.script -nt full_terminal.html ] && cat full_terminal.script | bash ansi2html.sh --bg=dark > full_terminal.html
-
-#while true; do
+while true; do
+  [ full_terminal.script -nt full_terminal.html ] && cat full_terminal.script | bash ansi2html.sh --bg=dark > full_terminal.html
   git add *
   git commit -m "${DATE}${COMMIT_MESSAGE}"
   git push -u origin master
-#  sleep 10s
-#done
+  sleep 10s
+done
 
